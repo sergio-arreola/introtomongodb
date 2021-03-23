cf1f7f0d2fbba40adcdedb96837f72da2a5a7bb5
Sergio Arreola <s.arreola84@gmail.com>
2021-03-22 21:07:32 -0700

    Week 3: Authentication

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("./models/user");
+
 const JwtStrategy = require("passport-jwt").Strategy;
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
@@ -33,5 +34,15 @@ exports.jwtPassport = passport.use(
     });
   })
 );
+
+exports.verifyAdmin = (req, res, next) => {
+  if (req.user.admin) {
+    return next();
+  } else {
+    const err = new Error("You are not authorized for this operation");
+    err.status = 403;
+    return next(err);
+  }
+};
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
