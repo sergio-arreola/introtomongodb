ff1e68b221497af839d371c927b6f57e77086d23
Sergio Arreola <s.arreola84@gmail.com>
2021-03-27 00:38:39 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
+const codes = require("./facebook/codes");
+
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: codes.facebookId,
+    clientSecret: codes.facebookSecret,
+  },
 };
